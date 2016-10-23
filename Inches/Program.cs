ae88bec50e04de7f9b8e691bc858327c82ff6236
uzver <funeral20@gmail.com>
2016-10-23 20:57:43 +0300

    Distance to the sun in inches

---
+++ Inches/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long inches;
+            long miles;
+
+            miles = 93000000;
+            inches = miles * 5280 * 12;
+            Console.WriteLine("Distance to the sun => " + inches + " inches");
+        }
+    }
+}
